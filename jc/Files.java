067f56df323c30ae2302288dc9f1b3eec7db547f
Max Reichardt <mreichardt@gmx.net>
2011-10-16 15:10:57 +0200

    Added lookup functions for files in finroc repositories.

--- jc/Files.java
+++ jc/Files.java
@@ -22,6 +22,7 @@
 package org.rrlib.finroc_core_utils.jc;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import org.rrlib.finroc_core_utils.jc.annotation.Const;
 import org.rrlib.finroc_core_utils.jc.annotation.InCpp;
@@ -30,15 +31,33 @@ import org.rrlib.finroc_core_utils.jc.annotation.Inline;
 import org.rrlib.finroc_core_utils.jc.annotation.NoCpp;
 import org.rrlib.finroc_core_utils.jc.annotation.Prefix;
 import org.rrlib.finroc_core_utils.jc.annotation.Ref;
+import org.rrlib.finroc_core_utils.xml.XMLDocument;
 
 /**
  * @author max
  *
  * Some basic file operations
+ * and lookup of files in finroc repositories.
  */
 @Inline @NoCpp @Prefix("s")
 @Include("<boost/filesystem.hpp>")
 public class Files {
+
+    /** Paths in which to search for finroc files */
+    private static ArrayList<String> pathsToCheck = new ArrayList<String>();
+
+    static {
+        String finrocHome = System.getenv("FINROC_HOME");
+        String projectHome = System.getenv("FINROC_PROJECT_HOME");
+        pathsToCheck.add(new File(".").getAbsolutePath() + "/");
+        if (projectHome != null) {
+            pathsToCheck.add(projectHome + "/");
+            pathsToCheck.add(projectHome + "/sources/java/");
+        }
+        if (finrocHome != null) {
+            pathsToCheck.add(finrocHome + "/");
+        }
+    }
 
     /**
      * Does file with specified name exist?
@@ -49,5 +68,40 @@ public class Files {
     @InCpp("return boost::filesystem::_exists(filename.getStdString());")
     public static boolean exists(@Const @Ref String filename) {
         return new File(filename).exists();
+    }
+
+    /**
+     * Lookup file in finroc repository.
+     *
+     * Searches in current path, $FINROC_PROJECT_HOME, $FINROC_HOME, $FINROC_HOME/sources/java, system installation (in this order).
+     *
+     * @param rawFilename Raw file name
+     * @return Filename to open (can possibly be temp file somewhere). "" if no file was found.
+     */
+    public static String getFinrocFile(String rawFilename) {
+        for (String path : pathsToCheck) {
+            if (exists(path + rawFilename)) {
+                return path + rawFilename;
+            }
+        }
+        return "";
+    }
+
+    /**
+     * Open XML document in finroc repository.
+     * (when dealing with archives, this can be more efficient than getFinrocFile which might create a temp file)
+     *
+     * Searches in current path, $FINROC_PROJECT_HOME, $FINROC_HOME, $FINROC_HOME/sources/java, system installation (in this order).
+     *
+     * @param rawFilename Raw file name.
+     * @param validate Whether the validation should be processed or not
+     * @return XML Document. Throws exception if file cannot be found.
+     */
+    public static XMLDocument getFinrocXMLDocument(String rawFilename, boolean validate) throws Exception {
+        String file = getFinrocFile(rawFilename);
+        if (file.length() == 0) {
+            throw new Exception("file not found");
+        }
+        return new XMLDocument(file);
     }
 }
